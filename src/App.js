6b0687d930ea0cb3610669601792a145c65e7c56
前田 <sloth19920731@gmail.com>
2022-11-07 23:43:00 +0900

    カスタムコンポーネントとpropsで動的かつ再利用性のあるコード

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <div className="App">
-      <h1> Hello, React! </h1>
+      <Person name={"John"} lastName={"A"} age={"10"} />
+      <Person name={"Hurry"} lastName={"B"} age={"20"} />
+      <Person name={"Micky"} lastName={"C"} age={"30"} />
     </div>
   );
 };
